e1ca0c422e24fd0041bc89fa0096c8e2596bd7f1
Josu Doncel Vicente <8248088+josudoncel@users.noreply.github.com>
2020-01-15 16:52:01 +0100

    Delete fractions.m

--- ternaryplotfiles/fractions.m
+++
@@ -1,8 +0,0 @@
-% FRACTIONS normalise ternary data
-%   [fA, fB, fC] = FRACTIONS(A, B, C) calculates fractional values for 
-
-function [fA, fB, fC] = fractions(A, B, C)
-Total = (A+B+C);
-fA = A./Total;
-fB = B./Total;
-fC = 1-(fA+fB);
